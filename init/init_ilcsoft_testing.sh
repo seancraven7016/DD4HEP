afd135469aa5953c8861500a7017b94274a981f8
Gabriel Penn <gp13181@bristol.ac.uk>
2016-07-14 10:25:18 +0100

    Implement the ILCSOFT variable for succinctness

--- init/init_ilcsoft_testing.sh
+++ init/init_ilcsoft_testing.sh
@@ -4,21 +4,21 @@ export ILCSOFT=/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09
 #--------------------------------------------------------------------------------
 #     LCCD
 #--------------------------------------------------------------------------------
-export LCCD="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/lccd/v01-03"
+export LCCD="$ILCSOFT/lccd/v01-03"
 
 
 #--------------------------------------------------------------------------------
 #     CondDBMySQL
 #--------------------------------------------------------------------------------
 export COND_DB_DEBUGLOG="/dev/stdout"
-export CondDBMySQL="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/CondDBMySQL/CondDBMySQL_ILC-0-9-6"
+export CondDBMySQL="$ILCSOFT/CondDBMySQL/CondDBMySQL_ILC-0-9-6"
 export LD_LIBRARY_PATH="$CondDBMySQL/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     LCIO
 #--------------------------------------------------------------------------------
-export LCIO="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/lcio/v02-07"
+export LCIO="$ILCSOFT/lcio/v02-07"
 export PYTHONPATH="$LCIO/src/python:$LCIO/examples/python:$PYTHONPATH"
 export PATH="$LCIO/tools:$LCIO/bin:$PATH"
 export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
@@ -27,7 +27,7 @@ export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     ROOT
 #--------------------------------------------------------------------------------
-export ROOTSYS="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/root/5.34.30"
+export ROOTSYS="$ILCSOFT/root/5.34.30"
 export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
@@ -42,22 +42,22 @@ export PATH="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/CMake/2.8.5/bin:$PATH"
 #--------------------------------------------------------------------------------
 #     ILCUTIL
 #--------------------------------------------------------------------------------
-export ilcutil="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/ilcutil/v01-02-01"
+export ilcutil="$ILCSOFT/ilcutil/v01-02-01"
 export LD_LIBRARY_PATH="$ilcutil/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     Marlin
 #--------------------------------------------------------------------------------
-export MARLIN="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Marlin/v01-08"
+export MARLIN="$ILCSOFT/Marlin/v01-08"
 export PATH="$MARLIN/bin:$PATH"
-export MARLIN_DLL="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinDD4hep/v00-01/lib/libMarlinDD4hep.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/DDMarlinPandora/v00-03/lib/libDDMarlinPandora.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinReco/v01-14/lib/libMarlinReco.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/PandoraAnalysis/v01-02-00/lib/libPandoraAnalysis.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinPandora/v02-02-00/lib/libMarlinPandora.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/LCFIVertex/v00-07-01/lib/libLCFIVertex.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/CEDViewer/v01-11/lib/libCEDViewer.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Overlay/v00-14/lib/libOverlay.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/FastJetClustering/v00-02/lib/libFastJetClustering.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinFastJet/v00-02/lib/libMarlinFastJet.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/LCTuple/v01-05/lib/libLCTuple.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinKinfit/v00-02/lib/libMarlinKinfit.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinKinfitProcessors/v00-01/lib/libMarlinKinfitProcessors.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinTrkProcessors/v02-02/lib/libMarlinTrkProcessors.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/ILDPerformance/v00-02/lib/libILDPerformance.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Clupatra/v00-13/lib/libClupatra.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Physsim/v00-02/lib/libPhyssim.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/FCalClusterer/v00-02/lib/libFCalClusterer.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/LCFIPlus/v00-07/lib/libLCFIPlus.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/ForwardTracking/v01-09/lib/libForwardTracking.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinTPC/v01-01/lib/libMarlinTPC.so:/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Garlic/v3.0.3/lib/libGarlic.so:$MARLIN_DLL"
+export MARLIN_DLL="$ILCSOFT/MarlinDD4hep/v00-01/lib/libMarlinDD4hep.so:$ILCSOFT/DDMarlinPandora/v00-03/lib/libDDMarlinPandora.so:$ILCSOFT/MarlinReco/v01-14/lib/libMarlinReco.so:$ILCSOFT/PandoraAnalysis/v01-02-00/lib/libPandoraAnalysis.so:$ILCSOFT/MarlinPandora/v02-02-00/lib/libMarlinPandora.so:$ILCSOFT/LCFIVertex/v00-07-01/lib/libLCFIVertex.so:$ILCSOFT/CEDViewer/v01-11/lib/libCEDViewer.so:$ILCSOFT/Overlay/v00-14/lib/libOverlay.so:$ILCSOFT/FastJetClustering/v00-02/lib/libFastJetClustering.so:$ILCSOFT/MarlinFastJet/v00-02/lib/libMarlinFastJet.so:$ILCSOFT/LCTuple/v01-05/lib/libLCTuple.so:$ILCSOFT/MarlinKinfit/v00-02/lib/libMarlinKinfit.so:$ILCSOFT/MarlinKinfitProcessors/v00-01/lib/libMarlinKinfitProcessors.so:$ILCSOFT/MarlinTrkProcessors/v02-02/lib/libMarlinTrkProcessors.so:$ILCSOFT/ILDPerformance/v00-02/lib/libILDPerformance.so:$ILCSOFT/Clupatra/v00-13/lib/libClupatra.so:$ILCSOFT/Physsim/v00-02/lib/libPhyssim.so:$ILCSOFT/FCalClusterer/v00-02/lib/libFCalClusterer.so:$ILCSOFT/LCFIPlus/v00-07/lib/libLCFIPlus.so:$ILCSOFT/ForwardTracking/v01-09/lib/libForwardTracking.so:$ILCSOFT/MarlinTPC/v01-01/lib/libMarlinTPC.so:$ILCSOFT/Garlic/v3.0.3/lib/libGarlic.so:$MARLIN_DLL"
 
 
 #--------------------------------------------------------------------------------
 #     CLHEP
 #--------------------------------------------------------------------------------
-export CLHEP="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/CLHEP/2.1.4.1"
+export CLHEP="$ILCSOFT/CLHEP/2.1.4.1"
 export CLHEP_BASE_DIR="$CLHEP"
 export CLHEP_INCLUDE_DIR="$CLHEP/include"
 export PATH="$CLHEP_BASE_DIR/bin:$PATH"
@@ -67,14 +67,14 @@ export LD_LIBRARY_PATH="$CLHEP_BASE_DIR/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     RAIDA
 #--------------------------------------------------------------------------------
-export RAIDA_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/RAIDA/v01-06-02"
+export RAIDA_HOME="$ILCSOFT/RAIDA/v01-06-02"
 export PATH="$RAIDA_HOME/bin:$PATH"
 
 
 #--------------------------------------------------------------------------------
 #     GEAR
 #--------------------------------------------------------------------------------
-export GEAR="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/gear/v01-05"
+export GEAR="$ILCSOFT/gear/v01-05"
 export PATH="$GEAR/tools:$GEAR/bin:$PATH"
 export LD_LIBRARY_PATH="$GEAR/lib:$LD_LIBRARY_PATH"
 
@@ -82,8 +82,8 @@ export LD_LIBRARY_PATH="$GEAR/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     DD4hep
 #--------------------------------------------------------------------------------
-export DD4hepINSTALL="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/DD4hep/v00-15"
-export DD4HEP="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/DD4hep/v00-15"
+export DD4hepINSTALL="$ILCSOFT/DD4hep/v00-15"
+export DD4HEP="$ILCSOFT/DD4hep/v00-15"
 export PYTHONPATH="$DD4HEP/python:$DD4HEP/DDCore/python:$PYTHONPATH"
 export PATH="$DD4HEP/bin:$PATH"
 export LD_LIBRARY_PATH="$DD4HEP/lib:$LD_LIBRARY_PATH"
@@ -92,7 +92,7 @@ export LD_LIBRARY_PATH="$DD4HEP/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     Geant4
 #--------------------------------------------------------------------------------
-export G4INSTALL="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/geant4/10.01"
+export G4INSTALL="$ILCSOFT/geant4/10.01"
 export G4ENV_INIT="$G4INSTALL/bin/geant4.sh"
 export G4SYSTEM="Linux-g++"
 
@@ -100,7 +100,7 @@ export G4SYSTEM="Linux-g++"
 #--------------------------------------------------------------------------------
 #     QT
 #--------------------------------------------------------------------------------
-export QTDIR="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/QT/4.7.4"
+export QTDIR="$ILCSOFT/QT/4.7.4"
 export QMAKESPEC="$QTDIR/mkspecs/linux-g++"
 export PATH="$QTDIR/bin:$PATH"
 export LD_LIBRARY_PATH="$QTDIR/lib:$LD_LIBRARY_PATH"
@@ -109,7 +109,7 @@ export LD_LIBRARY_PATH="$QTDIR/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     XercesC
 #--------------------------------------------------------------------------------
-export XercesC_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/xercesc/3.1.2"
+export XercesC_HOME="$ILCSOFT/xercesc/3.1.2"
 export PATH="$XercesC_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
 
@@ -123,7 +123,7 @@ export BOOST_ROOT="/cvmfs/ilc.desy.de/sw/boost/1.58.0"
 #--------------------------------------------------------------------------------
 #     GSL
 #--------------------------------------------------------------------------------
-export GSL_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/gsl/1.14"
+export GSL_HOME="$ILCSOFT/gsl/1.14"
 export PATH="$GSL_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$GSL_HOME/lib:$LD_LIBRARY_PATH"
 
@@ -131,28 +131,28 @@ export LD_LIBRARY_PATH="$GSL_HOME/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     CED
 #--------------------------------------------------------------------------------
-export PATH="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/CED/v01-09-02/bin:$PATH"
+export PATH="$ILCSOFT/CED/v01-09-02/bin:$PATH"
 export LD_LIBRARY_PATH="/cvmfs/ilc.desy.de/sw/x86_64_gcc44_sl6/v01-17-09/CED/v01-09-02/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     PandoraPFANew
 #--------------------------------------------------------------------------------
-export PANDORAPFANEW="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/PandoraPFANew/v02-05-00"
+export PANDORAPFANEW="$ILCSOFT/PandoraPFANew/v02-05-00"
 export LD_LIBRARY_PATH="$PANDORAPFANEW/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     Mokka
 #--------------------------------------------------------------------------------
-export MOKKA="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Mokka/mokka-08-05-01"
+export MOKKA="$ILCSOFT/Mokka/mokka-08-05-01"
 export PATH="$MOKKA/bin:$PATH"
 
 
 #--------------------------------------------------------------------------------
 #     MySQL
 #--------------------------------------------------------------------------------
-export MYSQL_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/mysql/5.0.45"
+export MYSQL_HOME="$ILCSOFT/mysql/5.0.45"
 export MYSQL_LIBDIR="$MYSQL_HOME/lib64/mysql"
 export MYSQL_PATH="$MYSQL_HOME"
 export MYSQL="$MYSQL_HOME"
@@ -163,8 +163,8 @@ export LD_LIBRARY_PATH="$MYSQL_HOME/lib64/mysql:$MYSQL_HOME/lib64:$MYSQL_HOME/li
 #--------------------------------------------------------------------------------
 #     CERNLIB
 #--------------------------------------------------------------------------------
-export CERN_ROOT="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/cernlib/2006"
-export CERN="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/cernlib"
+export CERN_ROOT="$ILCSOFT/cernlib/2006"
+export CERN="$ILCSOFT/cernlib"
 export CERN_LEVEL="2006"
 export CVSCOSRC="$CERN_ROOT/src"
 export PATH="$CERN_ROOT/bin:$PATH"
@@ -173,13 +173,13 @@ export PATH="$CERN_ROOT/bin:$PATH"
 #--------------------------------------------------------------------------------
 #     CEDViewer
 #--------------------------------------------------------------------------------
-export PATH="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/CEDViewer/v01-11/bin:$PATH"
+export PATH="$ILCSOFT/CEDViewer/v01-11/bin:$PATH"
 
 
 #--------------------------------------------------------------------------------
 #     FastJet
 #--------------------------------------------------------------------------------
-export FastJet_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/FastJet/3.1.2"
+export FastJet_HOME="$ILCSOFT/FastJet/3.1.2"
 export PATH="$FastJet_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$FastJet_HOME/lib:$LD_LIBRARY_PATH"
 
@@ -187,14 +187,14 @@ export LD_LIBRARY_PATH="$FastJet_HOME/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     KalTest
 #--------------------------------------------------------------------------------
-export KALTEST="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/KalTest/v02-01"
+export KALTEST="$ILCSOFT/KalTest/v02-01"
 export LD_LIBRARY_PATH="$KALTEST/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     aidaTT
 #--------------------------------------------------------------------------------
-export AIDATT="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/aidaTT/v00-03"
+export AIDATT="$ILCSOFT/aidaTT/v00-03"
 export PATH="$AIDATT/bin:$PATH"
 export LD_LIBRARY_PATH="$AIDATT/lib:$LD_LIBRARY_PATH"
 
@@ -202,28 +202,28 @@ export LD_LIBRARY_PATH="$AIDATT/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     GBL
 #--------------------------------------------------------------------------------
-export GBL="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/GBL/V01-16-04"
+export GBL="$ILCSOFT/GBL/V01-16-04"
 export LD_LIBRARY_PATH="$GBL/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     MarlinTPC
 #--------------------------------------------------------------------------------
-export MARLINTPC="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/MarlinTPC/v01-01"
+export MARLINTPC="$ILCSOFT/MarlinTPC/v01-01"
 export PATH="$MARLINTPC/bin:$PATH"
 
 
 #--------------------------------------------------------------------------------
 #     Druid
 #--------------------------------------------------------------------------------
-export DRUID="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/Druid/2.2"
+export DRUID="$ILCSOFT/Druid/2.2"
 export PATH="$DRUID/bin:$PATH"
 
 
 #--------------------------------------------------------------------------------
 #     HepPDT
 #--------------------------------------------------------------------------------
-export HepPDT_HOME="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/heppdt/3.04.01"
+export HepPDT_HOME="$ILCSOFT/heppdt/3.04.01"
 export PATH="$HepPDT_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$HepPDT_HOME/lib:$LD_LIBRARY_PATH"
 
@@ -231,14 +231,14 @@ export LD_LIBRARY_PATH="$HepPDT_HOME/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     GDML
 #--------------------------------------------------------------------------------
-export GDML="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/gdml/ilcsoft-v01-17-07"
+export GDML="$ILCSOFT/gdml/ilcsoft-v01-17-07"
 export LD_LIBRARY_PATH="$GDML/lib:$LD_LIBRARY_PATH"
 
 
 #--------------------------------------------------------------------------------
 #     LCDD
 #--------------------------------------------------------------------------------
-export LCDD="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/lcdd/ilcsoft-v01-17-07"
+export LCDD="$ILCSOFT/lcdd/ilcsoft-v01-17-07"
 export GDML_SCHEMA_DIR="$LCDD"
 export LD_LIBRARY_PATH="$LCDD/lib:$LD_LIBRARY_PATH"
 
@@ -246,7 +246,7 @@ export LD_LIBRARY_PATH="$LCDD/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     SLIC
 #--------------------------------------------------------------------------------
-export SLIC="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/slic/ilcsoft-v01-17-07"
+export SLIC="$ILCSOFT/slic/ilcsoft-v01-17-07"
 export PATH="$SLIC/build/bin:$PATH"
 export LD_LIBRARY_PATH="$SLIC/build/lib:$LD_LIBRARY_PATH"
 
@@ -254,7 +254,7 @@ export LD_LIBRARY_PATH="$SLIC/build/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     SlicPandora
 #--------------------------------------------------------------------------------
-export SLICPANDORA="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/slicPandora/ilcsoft-v01-17-07"
+export SLICPANDORA="$ILCSOFT/slicPandora/ilcsoft-v01-17-07"
 export PATH="$SLICPANDORA/build/bin:$PATH"
 export LD_LIBRARY_PATH="$SLICPANDORA/build/lib:$LD_LIBRARY_PATH"
 
@@ -262,7 +262,7 @@ export LD_LIBRARY_PATH="$SLICPANDORA/build/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     lcgeo
 #--------------------------------------------------------------------------------
-export lcgeo_DIR="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/lcgeo/v00-07"
+export lcgeo_DIR="$ILCSOFT/lcgeo/v00-07"
 export PYTHONPATH="$lcgeo_DIR/lib/python:$PYTHONPATH"
 export PATH="$lcgeo_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$lcgeo_DIR/lib:$LD_LIBRARY_PATH"
@@ -271,7 +271,7 @@ export LD_LIBRARY_PATH="$lcgeo_DIR/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #     DD4hepExamples
 #--------------------------------------------------------------------------------
-export DD4hepExamples="/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-09/DD4hepExamples/v00-15"
+export DD4hepExamples="$ILCSOFT/DD4hepExamples/v00-15"
 export PATH="$DD4hepExamples/bin:$PATH"
 export LD_LIBRARY_PATH="$DD4hepExamples/lib:$LD_LIBRARY_PATH"
 
