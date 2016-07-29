3b2950076bcf6febb06bb29f7092686bd3f771df
Gabriel Penn <gp13181@bristol.ac.uk>
2016-07-29 10:20:52 +0100

    Update residual.py

--- analysis/residual.py
+++ analysis/residual.py
@@ -97,7 +97,7 @@ def parparRes(posData, layer, par1, par2):
 			zFit = (posData[res][par1] - szGraph.GetFunction("pol1").GetParameter(0)) / szGraph.GetFunction("pol1").GetParameter(1)
 			residual = zFit - zActual
 			#print "Residual:", residual
-			#if fabs(residual) < 1: 
+			if fabs(residual) < 1: 
 				residuals.append(residual)
 
 		szGraph.Delete()
